a57f73bca43197e7db797d9b6c60f9894b50fbf2
Sergio Silva <sertykos@gmail.com>
2015-01-21 18:07:41 +0100

    21/01/2015

---
+++ vendor/assets/stylesheets/index.js
@@ -0,0 +1,6 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+var slider = document.getElementById("slider");
+var sliderImg = slider.childNodes[1].childNodes[1];
+sliderImg.setAttribute("style", "box-shadow: 1px 1px 5px white");
